5ccb51f79e6ade9cf7f77c4b5e0ad55ed24577e5
Esa Heikkinen <eras.rasmuson@gmail.com>
2018-03-20 22:34:50 +0200

    LogDig linux script

---
+++ LogDig/LogDig_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Start"
+
+MY_PATH="/home/esa/projects/LA"
+
+SCRIPT_PATH=${MY_PATH}"/LogDig/"
+LOGS_PATH=${MY_PATH}"/LogFile/PreProsessed/TLG_1/"
+RESULT_PATH=${MY_PATH}"/LogRes/TLG_1/"
+ANALYZE_PATH=${MY_PATH}"/LogAna/"
+ANALYZE_FILE="logdig_TLG_1_new4"
+
+python ${SCRIPT_PATH}LogDig.py \
+						-date 24.02.2016 \
+						-start_time 08:00:00 \
+						-stop_time 09:00:00 \
+						-input_logs_path $LOGS_PATH \
+						-input_ssd_path $LOGS_PATH \
+						-output_files_path $RESULT_PATH \
+						-analyze_file_path $ANALYZE_PATH \
+						-analyze_file $ANALYZE_FILE \
+						-analyze_file_mode NEW \
+						-analyzing_mode NORMAL:0 \
+						-gui_enable 1 \
+						-gui_seq_draw_mode "time"
+
+#						-analyzing_mode COMPARE:3 ^
+#						-analyzing_mode NORMAL:0 ^
+#						-gui_seq_draw_mode "search"
