d7924387ac3ffdd0c845246779fe1247e6616e1d
ErasRasmuson <eras.rasmuson@gmail.com>
2018-07-05 16:26:02 +0300

    Taxi_LongRides_Big example case was added..

---
+++ LogAna/Taxi_LongRides_Big.py
@@ -0,0 +1,91 @@
+# -*- coding: cp1252 -*-
+"""
+###############################################################################
+HEADER:     Taxi_LongRides.py
+
+AUTHOR:     Esa Heikkinen
+DATE:       26.06.2018
+DOCUMENT:   -
+VERSION:    "$Id$"
+REFERENCES: -
+PURPOSE:
+CHANGES:    "$Log$"
+###############################################################################
+"""
+from logdig_analyze_template import *
+
+# ----------------------------- DATA-DRIVEN PART -----------------------------
+VARIABLES = {
+	"STARTTIME-DATE":	"2013-01-01",
+	"STARTTIME-TIME":	"00:00:00",
+	"STOPTIME-DATE":	"2013-01-30",
+	"STOPTIME-TIME": 	"23:59:59"
+	}
+START = {
+	"state":   "BEGIN",
+	"func":   "start"
+	}
+ESU["BEGIN"] = {
+	"esu_mode":             "SEARCH_EVENT:First:NextRow",
+	"log_filename_expr":    "TaxiRides.csv",
+	"log_varnames":         "isStart=START",
+	"log_timecol_name":     "startTime",
+	"log_start_time_expr":  "<STARTTIME-BEGIN>,0",
+	"log_stop_time_expr":   "<STOPTIME>,0",
+
+	"TF_state":    "END",
+	"TF_func":     "found_begin",
+	"TN_state":    "STOP",
+	"TN_func":     "exit_normal",
+	"TE_state":    "STOP",
+	"TE_func":     "exit_error",
+	"GUI_line_num":	"0"
+}
+ESU["END"] = {
+	"esu_mode":             "SEARCH_EVENT:First",
+	"log_filename_expr":    "TaxiRides.csv_<SET-RIDEID>",
+	"log_varnames":         "isStart=END",
+	"log_timecol_name":     "startTime",
+	"log_start_time_expr":  "<startTime>,0",
+	"log_stop_time_expr":   "<startTime>,7200",
+
+	"TF_state":    "BEGIN",
+	"TF_func":     "found_end",
+	"TN_state":    "BEGIN",
+	"TN_func":     "not_found_end",
+	"TE_state":    "STOP",
+	"TE_func":     "exit_error",
+	"GUI_line_num":	"1"
+}
+STOP = {
+	"func":     ""
+}
+
+# ----------------------------- FUNCTION PART -----------------------------
+def start():
+	set_datetime_variable("STARTTIME","STARTTIME-DATE","STARTTIME-TIME")
+	set_datetime_variable("STOPTIME","STOPTIME-DATE","STOPTIME-TIME")
+	set_sbk_file("Taxi_LongRides_Big","SET-RIDEID","startTime","endTime")
+	copy_variable("STARTTIME-BEGIN","STARTTIME")
+
+	logfiles_data.read("/home/esa/projects/LA/LogFile/PreProsessed/TaxiRides/TaxiRides.csv","startTime")
+	logfiles_data.transform_operation_keyby("/home/esa/projects/LA/LogFile/PreProsessed/TaxiRides/TaxiRides.csv","rideId")
+
+def found_begin():
+	print("found_begin")
+	copy_variable("SET-RIDEID","rideId")
+	copy_variable("STARTTIME-BEGIN","startTime")
+
+def found_end():
+	print("found_end")
+
+def not_found_end():
+	print("not_found_end")
+	copy_variable("STARTTIME-BEGIN","startTime")
+	print_sbk_file()
+
+def exit_normal():
+	print("exit_normal")
+
+def exit_error():
+	print("exit_error")
---
+++ LogDig/Taxi_LongRides_Big.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Start"
+
+MY_PATH="/home/esa/projects/LA"
+
+SCRIPT_PATH=${MY_PATH}"/LogDig/"
+LOGS_PATH=${MY_PATH}"/LogFile/PreProsessed/TaxiRides/"
+RESULT_PATH=${MY_PATH}"/LogRes/TaxiRides/"
+ANALYZE_PATH=${MY_PATH}"/LogAna/"
+ANALYZE_FILE="Taxi_LongRides_Big"
+
+python ${SCRIPT_PATH}LogDig.py \
+		-date 01.01.2013 \
+		-start_time 00:00:00 \
+		-stop_time 23:59:59 \
+		-input_logs_path $LOGS_PATH \
+		-input_ssd_path $LOGS_PATH \
+		-output_files_path $RESULT_PATH \
+		-analyze_file_path $ANALYZE_PATH \
+		-analyze_file $ANALYZE_FILE \
+		-analyze_file_mode NEW \
+		-analyzing_mode NORMAL:0 \
+		-gui_enable 0 \
+		-gui_seq_draw_mode "time"
+
+#		-analyzing_mode COMPARE:3 ^
+#		-analyzing_mode NORMAL:0 ^
+#		-gui_seq_draw_mode "search"
